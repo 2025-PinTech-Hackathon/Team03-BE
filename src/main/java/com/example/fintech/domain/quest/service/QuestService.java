09b0a61db24a51e48799ac2ad13af6f62c5f5b0a
Haemin Kim <haemin1230@gmail.com>
2025-05-16 03:18:39 +0900

    ✨ (#4) 퀘스트 service 추가

--- src/main/java/com/example/fintech/domain/quest/service/QuestService.java
+++ src/main/java/com/example/fintech/domain/quest/service/QuestService.java
@@ -1,4 +1,8 @@
 package com.example.fintech.domain.quest.service;
 
+import com.example.fintech.domain.quest.dto.request.QuestRequestDTO;
+import com.example.fintech.domain.quest.dto.response.QuestResponseDTO;
+
 public interface QuestService {
+    QuestResponseDTO createQuest(String token, QuestRequestDTO request);
 }
--- src/main/java/com/example/fintech/domain/quest/service/QuestServiceImpl.java
+++ src/main/java/com/example/fintech/domain/quest/service/QuestServiceImpl.java
@@ -1,4 +1,43 @@
 package com.example.fintech.domain.quest.service;
 
-public class QuestServiceImpl {
+import com.example.fintech.domain.quest.converter.QuestConverter;
+import com.example.fintech.domain.quest.dto.request.QuestRequestDTO;
+import com.example.fintech.domain.quest.dto.response.QuestResponseDTO;
+import com.example.fintech.domain.quest.entity.Quest;
+import com.example.fintech.domain.quest.exception.QuestErrorCode;
+import com.example.fintech.domain.quest.exception.QuestException;
+import com.example.fintech.domain.quest.repository.QuestRepository;
+import com.example.fintech.domain.user.entity.User;
+import com.example.fintech.domain.user.repository.UserRepository;
+import com.example.fintech.global.security.jwt.CustomJwtUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+
+
+@Service
+@RequiredArgsConstructor
+public class QuestServiceImpl implements QuestService {
+
+    private final QuestRepository questRepository;
+    private final QuestConverter questConverter;
+    private final CustomJwtUtil CustomJwtUtil;
+    private final UserRepository userRepository;
+
+    @Override
+    public QuestResponseDTO createQuest(String token, QuestRequestDTO request) {
+        Long userId = CustomJwtUtil.getUserId(token);
+
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new QuestException(QuestErrorCode.USER_NOT_FOUND));
+
+        if (!request.getDeadline().matches("\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}")) {
+            throw new QuestException(QuestErrorCode.INVALID_DATE_FORMAT);
+        }
+
+        Quest quest = questConverter.toEntity(request, user);
+        questRepository.save(quest);
+
+        return questConverter.toResponse(quest);
+    }
 }
